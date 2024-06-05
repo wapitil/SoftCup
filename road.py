dd99fd4a2a50656225c976b71f160beaa9b90e3c
wapitil <wapiti168@gmail.com>
2024-06-06 00:39:07 +0800

    删除文件

--- road.py
+++
@@ -1,5 +0,0 @@
-import random
-city = ["江岸区", "江汉区", "硚口区", "汉阳区", "武昌区", "青山区", "洪山区","蔡甸区","江夏区","黄陂区","新洲区","东西湖区","汉南区"]
-road1, road2 = random.sample(city, 2)
-print(f"从{road1}到{road2}")
-# print(road2)
